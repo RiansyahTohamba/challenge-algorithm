626cf362995a056bc76894be1266fd5fbddc16fe
riansyah <mriansyah93@gmail.com>
2022-05-27 08:17:17 +0700

    add notes for migratory bird case

--- frequency/migratory-bird/main.go
+++ frequency/migratory-bird/main.go
@@ -4,22 +4,23 @@ import "fmt"
 
 func migratoryBirds(arr []int32) int32 {
 	// alokasi array dengan len=6 (0-5)
-	counts := make([]int, 6)
-	// index pada array counts adalah birdId
+	mapBird := make([]int, 6)
+	// index pada array mapBird adalah birdId
 	// value nya adalah total kemunculan birdId pada set
 	for _, birdId := range arr {
-		counts[birdId]++
+		mapBird[birdId]++
 	}
-	fmt.Println(counts)
-	// mencari maksimum frequency
-	maxFreq := counts[0]
-	for _, freq := range counts {
+	// fmt.Println(mapBird)
+	// inisiasi maxFrequencies dengan nilai pertama dari arr
+	maxFreq := mapBird[arr[0]]
+
+	for _, freq := range mapBird {
 		if freq > maxFreq {
 			maxFreq = freq
 		}
 	}
 	// temukan minIdx
-	for minIdx, freq := range counts {
+	for minIdx, freq := range mapBird {
 		if freq == maxFreq {
 			return int32(minIdx)
 		}
@@ -28,9 +29,8 @@ func migratoryBirds(arr []int32) int32 {
 }
 
 func main() {
-	// case dimana
-	// id burung harus masuk dalam set = {1,2,3,4,5}
-	// tidak boleh diluar itu
+	// case dimana id burung harus masuk dalam range (1..5) = {1,2,3,4,5}
+	// tidak boleh diluar itu (diatas 5,6,...)
 
 	birds := []int32{1, 1, 1, 2, 2, 2, 3, 4}
 
