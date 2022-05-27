5b64d75c3e2f35a5962bb0d902aae020f7da0571
riansyah <mriansyah93@gmail.com>
2022-05-27 16:38:37 +0700

    skip string problem

--- string/ignore-white-space/main.go
+++ string/ignore-white-space/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+)
+
 // ("    United Kingdom    ")
 // ("united-kingdom")
 // ""
+var jawaban = "United Kingdom"
+
+func main() {
+	userInput := "    United    Kingdom    "
+	reg, _ := regexp.Compile(`(\w+)(?:\s*)(\w+)`)
+	// (?:\s*) exclude space?
+	// ketika di compile, yang diambil hanya (\d+),([\+\-\*\/]),(\d+)
+	match := reg.FindStringSubmatch(userInput)
+	for _, v := range match {
+		fmt.Println(v)
+	}
+	fmt.Println(match[0])
+
+}
