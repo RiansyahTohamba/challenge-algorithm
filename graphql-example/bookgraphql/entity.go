0ec3f8bc4bb885df9a293d1a7433af37e69694e9
riansyah <mriansyah93@gmail.com>
2022-07-22 09:30:01 +0700

    init graphql for book use case via web api

--- graphql-example/bookgraphql/entity.go
+++ graphql-example/bookgraphql/entity.go
@@ -1,7 +1,5 @@
 package main
 
-var books []Book
-
 type Book struct {
 	Id       string
 	Title    string
@@ -10,11 +8,12 @@ type Book struct {
 	Category string
 }
 
-func seed() {
-	books = []Book{
+func populate() []Book {
+	books := []Book{
 		{"102391123", "Harry Potter 1", "JK Rowling", 100000.0, "fiction"},
 		{"102391242", "Harry Potter 2", "JK Rowling", 300000.0, "fiction"},
 		{"102391111", "Harry Potter 3", "JK Rowling", 300000.0, "fiction"},
 		{"102391231", "Harry Potter 4", "JK Rowling", 300000.0, "fiction"},
 	}
+	return books
 }
