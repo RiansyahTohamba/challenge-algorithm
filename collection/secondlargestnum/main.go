c8200248ae7cec4df57e4b7cbb2b1e8038d8603e
riansyah <mriansyah93@gmail.com>
2022-07-28 06:42:57 +0700

    add notes for searching rank 1 and 2

--- collection/secondlargestnum/main.go
+++ collection/secondlargestnum/main.go
@@ -4,20 +4,32 @@ import "fmt"
 
 func main() {
 	list := []int{20, 9, 7, 2, 30}
-	high1, high2 := getFirstSecondRank(list)
-	fmt.Println(high1)
-	fmt.Println(high2)
+	firstRank, secondRank := getFirstSecondRank(list)
+	fmt.Println(firstRank)
+	fmt.Println(secondRank)
+
 }
 
 // O(n)
-func getFirstSecondRank(nums []int) (high1, high2 int) {
+func getFirstSecondRank(nums []int) (firstRank, secondRank int) {
 	for _, num := range nums {
-		if num > high1 {
-			high2 = high1
-			high1 = num
-		} else if num != high1 && num > high2 {
-			high2 = num
+		// is new ranking 1?
+		if num > firstRank {
+			// ranking 2 diganti oleh ranking 1 sebelumnya
+			secondRank = firstRank
+			// ranking 1 digeser oleh elemen pada array
+			firstRank = num
+		} else if num != firstRank && num > secondRank {
+			// jika num=firstRank, maka ranking 2 sama dengan ranking 1
+			// ranking 2 diganti oleh bilangan num besar selain firstRank
+			secondRank = num
 		}
 	}
 	return
 }
+
+// pada algoritma ini, kita tidak perlu mengurutkan semua bilangan hingga terurut sempurna
+// jadi tidak perlu sorting untuk mencari rank 1 dan rank 2
+// sorting is overkill
+
+// if - if lebih lambat dibanding if - elseif
